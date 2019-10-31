e66399cbc2f53ecfcf2a9bd6b03c97c675344f53
Kevin GUILLARD <stolcraft@gmail.com>
2019-10-31 15:00:30 +0100

    first commit

---
+++ src/main/java/master_ensup_Kevin/master_ensup_Kevin/App.java
@@ -0,0 +1,13 @@
+package master_ensup_Kevin.master_ensup_Kevin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
